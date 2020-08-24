f17686c4685211a97645267a7277927e381b80a5
Aman Thukral <thukral618@gmail.com>
2020-08-24 23:04:47 +0530

    updating home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      Google
-      {/* Homepage */}
-      <Home />
-      {/* Searchpage */}
+
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>Search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+
+
+      </Router>
+
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Home.css';
+import { Link } from 'react-router-dom';
 function Home() {
     return (
         <div className="home">
-            <h1>Homepage</h1>
+            <div className="home__header">
+                <div className="home__headerLeft">
+                    <Link to="about">about</Link>
+                    <Link to="store">Store</Link>
+                </div>
+                <div className="home_headerRight">
+                    <Link to="gmail">Gmail</Link>
+                    <Link to="images">Images</Link>
+                    {/*  */}
+                    {/*  */}
+                </div>
+            </div>
+            <div className="home__body">
 
+            </div>
         </div>
     )
 }
