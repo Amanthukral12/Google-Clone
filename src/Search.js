2ef92ae26e52072543c9a77f15422ab6f2cdea38
Aman Thukral <thukral618@gmail.com>
2020-08-25 02:36:02 +0530

    updating home page

--- src/Search.js
+++ src/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Search.css';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
+import { Button } from '@material-ui/core';
 function Search() {
     return (
         <div className="search">
@@ -9,6 +10,10 @@ function Search() {
                 <SearchIcon className="search__inputIcon" />
                 <input />
                 <MicIcon />
+            </div>
+            <div className="search__buttons">
+                <Button variant="outlined">Google Search</Button>
+                <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
         </div>
     )
