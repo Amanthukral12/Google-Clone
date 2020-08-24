95de3f31fe48888db45ea01f9edd0281c837e2af
Aman Thukral <thukral618@gmail.com>
2020-08-25 02:04:06 +0530

    Styling home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import './Home.css';
 import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
+import Search from '../Search';
 function Home() {
     return (
         <div className="home">
             <div className="home__header">
                 <div className="home__headerLeft">
-                    <Link to="about">about</Link>
+                    <Link to="about">About</Link>
                     <Link to="store">Store</Link>
                 </div>
                 <div className="home_headerRight">
                     <Link to="gmail">Gmail</Link>
                     <Link to="images">Images</Link>
-                    {/*  */}
-                    {/*  */}
+                    <AppsIcon />
+                    <Avatar />
                 </div>
             </div>
             <div className="home__body">
-
+                <img src="http://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
+                <div className="home__inputContainer">
+                    <Search />
+                </div>
             </div>
         </div>
     )
