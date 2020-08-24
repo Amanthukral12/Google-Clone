94670637ef2aa16fb7a21a4bd71b79c327b931f1
Aman Thukral <thukral618@gmail.com>
2020-08-24 19:36:23 +0530

    adding Homepage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="app">
       Google
       {/* Homepage */}
+      <Home />
       {/* Searchpage */}
     </div>
   );
