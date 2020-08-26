3561e5bad52be0c66e52e1d20aad7f165bfcd7ed
Aman Thukral <thukral618@gmail.com>
2020-08-27 03:03:36 +0530

    adding searchpage components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchPage from './SearchPage';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
     <div className="app">
-
       <Router>
         <Switch>
           <Route path="/search">
             <SearchPage />
-
           </Route>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
         </Switch>
-
-
-
       </Router>
-
     </div>
   );
 }
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,6 +5,12 @@ import useGoogleSearch from "./useGoogleSearch";
 import Response from "./response";
 import { Link } from "react-router-dom";
 import Search from "./Search";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
   // {Live api call}
@@ -24,9 +30,53 @@ function SearchPage() {
 
         <div className="searchPage__headerBody">
           <Search hideButtons />
+          <div className="searchPage__options">
+            <div className="searchPage__optionsLeft">
+              <div className="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage__option">
+                <DescriptionIcon />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage__option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage__option">
+                <LocalOfferIcon />
+                <Link to="/shopping">Shopping</Link>
+              </div>
+              <div className="searchPage__option">
+                <RoomIcon />
+                <Link to="/maps">Maps</Link>
+              </div>
+              <div className="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/more">More</Link>
+              </div>
+            </div>
+            <div className="searchPage__optionsRight">
+              <div className="searchPage__option">
+                <Link to="/settings">Settings</Link>
+              </div>
+              <div className="searchPage__option">
+                <Link to="/tools">Tools</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="searchPage__results"></div>
+
+      {true && (
+        <div className="searchPage__results">
+          <p className="searchPage_resultCount">
+            About {data?.searchInformation.formattedTotalResults} results (
+            {data?.searchInformation.formattedSearchTime} seconds) for Tesla
+          </p>
+        </div>
+      )}
     </div>
   );
 }
