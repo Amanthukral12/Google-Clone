7a303b99ee3b84d9816aef3846e48e611ab949d5
Aman Thukral <thukral618@gmail.com>
2020-08-26 22:38:21 +0530

    Setting up searchpage and styling it

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,21 +1,34 @@
-import React from 'react'
-import './SearchPage.css';
-import { useStateValue } from './StateProvider';
-import useGoogleSearch from './useGoogleSearch';
+import React from "react";
+import "./SearchPage.css";
+import { useStateValue } from "./StateProvider";
+import useGoogleSearch from "./useGoogleSearch";
+import Response from "./response";
+import { Link } from "react-router-dom";
+import Search from "./Search";
 function SearchPage() {
-    const [{ term }, dispatch] = useStateValue();
-    const { data } = useGoogleSearch(term);
-    console.log(data);
-    return (
-        <div className="searchPage">
-            <div className="searchPage__header">
-                <h1>{term}</h1>
-            </div>
-            <div className="searchPage__results">
+  const [{ term }, dispatch] = useStateValue();
+  // {Live api call}
+  // const { data } = useGoogleSearch(term);
+  const data = Response;
+  console.log(data);
+  return (
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <Link to="/">
+          <img
+            className="searchPage__logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt=""
+          />
+        </Link>
 
-            </div>
+        <div className="searchPage__headerBody">
+          <Search hideButtons />
         </div>
-    )
+      </div>
+      <div className="searchPage__results"></div>
+    </div>
+  );
 }
 
-export default SearchPage
+export default SearchPage;
